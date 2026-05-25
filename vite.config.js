e09462ed498594581bcae2b64d80214571bfe663
Fauzan Azima <azzamfauzan21@gmail.com>
2026-05-25 10:37:29 +0700

    Update base URL for new repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/the-tumpuls/',
+  base: '/the-tumpul-s/',
   plugins: [react()],
   server: {
     port: 5193
