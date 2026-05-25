5a0fb3b3bcc87b4fe50c4b748aea8d25449fe4ff
Fauzan Azima <azzamfauzan21@gmail.com>
2026-05-25 11:41:12 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tumpuls/',
+  base: '/web-tumpuls/',
   plugins: [react()],
   server: {
     port: 5193
