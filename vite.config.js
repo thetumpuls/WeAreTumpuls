76aa24930b8f64a5b44730398001ba7d655475d6
Fauzan Azima <azzamfauzan21@gmail.com>
2026-05-25 10:52:40 +0700

    chore: update base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/the-tumpul-s/',
+  base: '/the-tumpuls/',
   plugins: [react()],
   server: {
     port: 5193
